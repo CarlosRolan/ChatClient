7f442f6d0ae33cb74f9e026cc77533a7f58c30a3
Carlos Rolán Díaz <carlosrolandiaz@gmail.com>
2023-07-24 10:10:06 +0200

    REF / ADD Refractoring the console COdes and adding the Chat state params

--- src/controller/console/ConsoleActions.java
+++ src/controller/console/ConsoleActions.java
@@ -24,7 +24,7 @@ public interface ConsoleActions {
     }
 
     default void clearConsole() {
-        System.out.print(CLEAR_CONSOLE);
+        System.out.print(CLI_CLEAR_CONSOLE);
         System.out.print(ConsoleColor.DEFAULT.getColor());
         System.out.flush();
     }
@@ -57,59 +57,65 @@ public interface ConsoleActions {
         System.out.print(ansiCode);
     }
 
-    final String CLEAR_CONSOLE = "\033[H\033[2J";
+    // Console Intro
+    final String INTRO = "==WELLCOME==\nThis chat was created by Carlos Rolán.\nThis app is handled by a server and all connections are supervised by it.";
 
+    // CONSOLE ACTIONS
+    final String CLI_CLEAR_CONSOLE = "\033[H\033[2J";
+
+    // CONSOLE OPTIONS
     final String OP_1 = "1";
     final String OP_2 = "2";
     final String OP_3 = "3";
     final String OP_4 = "4";
     final String OP_5 = "5";
     final String OP_6 = "6";
-
-    final String OP_YES = "a";
-    final String OP_NO = "b";
-
+    final String OP_ALLOW = "a";
+    final String OP_DENY = "b";
     final String OP_EXIT = "0";
-
-    // Console Intro
-    final String INTRO = "==WELLCOME==\nThis chat was created by Carlos Rolán.\nThis app is handled by a server and all connections are supervised by it.";
+    final String OP_SINGLE_EXIT = ".exit";
 
     // User Input Actions
-    final String ACTION_SET_NICK = ">Write a nickname to identify yourself";
-    final String ACTION_SELECT_USER_BY_ID = ">Select an ID of the user you want to chat with [id]";
+    final String IN_SET_NICK = ">Write a nickname to identify yourself";
+    final String IN_SELECT_USER = ">Select an ID of the user you want to chat with [id]";
 
-    final String ACTION_CHAT_SELECT = ">Select an avaliable CHAT to start chatting";
-    final String ACTION_CHAT_SELECT_USER = ">Select an ID of the CHAT you want to chat with";
+    final String IN_SELECT_CHAT = ">Select an ID of the CHAT you want enter";
 
-    final String ACTION_SEND_MSG_TO_SINGLE = ">Write a msg to";
-    final String ACTION_EXIT_SINGLE = ".exit";
-    final String ACTION_SELECT_SINGLE = ">Please select a number from the list";
+    final String IN_SELECT_SINGLE = ">Please select a number from the list";
 
-    // Console OCHAT
-    final String MENU_OP_1 = "1.Show online users";
-    final String MENU_OP_2 = "2.Start single conversation";
-    final String MENU_OP_2_1 = "a.Select by ID";
-    final String MENU_ALLOW_SINGLE = "\ta.Allow";
-    final String MENU_DENY_SINGLE = "\tb.Deny";
-    final String MENU_OP_3 = "3.Chats";
-    final String MENU_OP_EXIT = "0.Exit";
+    // MAIN MENU
+    final String MENU_MAIN_1 = "1.Show online users";
+    final String MENU_MAIN_2 = "2.Start single conversation";
+    final String MENU_MAIN_2_1 = "a.Select by ID";
+    final String MENU_MAIN_3 = "3.Chats";
+    final String MENU_MAIN_EXIT = "0.Exit";
 
-    final String MENU_CHAT_1 = "\t1.Send msg";
-    final String MENU_CHAT_2 = "\t2.Add member";
-    final String MENU_CHAT_3 = "\t3.Delete member";
-    final String MENU_CHAT_4 = "\t4.Manage Permissions";
-    final String MENU_CHAT_4_1 = "\ta.Make Admin";
-    final String MENU_CHAT_4_2 = "\tb.Make Cooperator";
-    final String MENU_CHAT_4_3 = "\tc.Make Regular";
-    final String MENU_CHAT_5 = "\t5.Show Members";
+    // SINGLE MENU
+    final String MENU_SINGLE_SEND_MSG = ">Write a msg to";
+    final String MENU_SINGLE_EXIT = ">To exit write '" + OP_SINGLE_EXIT + "'";
+    final String MENU_SINGLE_ALLOW = "\ta.Allow";
+    final String MENU_SINGLE_DENY = "\tb.Deny";
+
+    // CHAT MENU
+    final String MENU_CHAT_1 = "\t1.Enter chat";
+    final String MENU_CHAT_2 = "\t2.Create chat";
+    final String MENU_CHAT_3 = "\t3.Delete chat";
+
+    final String MENU_CHAT_1_1 = "\t1.Send msg to chat";
+    final String MENU_CHAT_1_2 = "\t2.Manage chat";
+    final String MENU_CHAT_1_2_1 = "\t1.Show Members";
+    final String MENU_CHAT_1_2_2 = "\t2.Add Member";
+    final String MENU_CHAT_1_2_3 = "\t3.Delete Memeber";
     final String MENU_CHAT_EXIT = "\t0.Exit chat";
+
     final String MENU_OP_ERROR = ">Option incorrect";
 
+    // CONFIG CONSOLE
     final String DATE_FORMAT = "HH:mm:ss";
 
+    // FEEDBACK TO USER
     final String _PENDING_TO_ACCEPT = " pending to accept";
     final String _ACCEPTS_THE_INVITATION = " accepts the invitation";
     final String _LEFT_SINGLE_CHAT = " has left the chat, press ENTER to back to the MAIN MENU";
 
-    final String _EXIT_SINGLE = ">To exit write '" + ACTION_EXIT_SINGLE + "'";
 }
--- src/controller/console/ConsoleConnection.java
+++ src/controller/console/ConsoleConnection.java
@@ -9,12 +9,13 @@ import java.util.Scanner;
 import com.ApiCodes;
 import com.Connection;
 import com.Msg;
+import com.chat.Chat;
 
 import api.ClientAPI;
 
 public class ConsoleConnection extends Thread implements ApiCodes, ConsoleActions {
 
-    enum ConnectionState {
+    enum ConsoleState {
         IN_SINGLE_CHAT,
         IN_CHAT,
     }
@@ -33,10 +34,15 @@ public class ConsoleConnection extends Thread implements ApiCodes, ConsoleAction
     }
     // --------
 
+    private final Connection c;
+
+    // Console state vars
     private boolean inSingleChat = false;
-    private Connection c = null;
     private String singleId = "0";
     private String singleNick = null;
+
+    private boolean inChat = false;
+    private Chat currentChat;
 
     // getters
     public Connection getConnection() {
@@ -47,9 +53,17 @@ public class ConsoleConnection extends Thread implements ApiCodes, ConsoleAction
         return inSingleChat;
     }
 
+    public boolean isInChat() {
+        return inChat;
+    }
+
+    public Chat getCurrentChat() {
+        return currentChat;
+    }
+
     // constructor
     private ConsoleConnection() {
-        System.out.println(ACTION_SET_NICK);
+        System.out.println(IN_SET_NICK);
         c = new Connection(sc.nextLine());
     }
 
@@ -61,36 +75,64 @@ public class ConsoleConnection extends Thread implements ApiCodes, ConsoleAction
 
     private void showMainMenu() {
         System.out.println("==========|" + c.getNick() + "|==========");
-        System.out.println(MENU_OP_1);
-        System.out.println(MENU_OP_2);
-        System.out.println(MENU_OP_3);
-        System.out.println(MENU_OP_EXIT);
+        System.out.println(MENU_MAIN_1);
+        System.out.println(MENU_MAIN_2);
+        System.out.println(MENU_MAIN_3);
+        System.out.println(MENU_MAIN_EXIT);
     }
 
     private void showSingleMenu() {
-        System.out.println(ACTION_SEND_MSG_TO_SINGLE);
-        System.out.println(_EXIT_SINGLE);
+        System.out.println(MENU_SINGLE_SEND_MSG);
+        System.out.println(MENU_SINGLE_EXIT);
+    }
+
+    private void showChatMenu() {
+        System.out.println(MENU_CHAT_1);
+        System.out.println(MENU_CHAT_2);
+        System.out.println(MENU_CHAT_3);
+        System.out.println(MENU_CHAT_EXIT);
+    }
+
+    private void showInChatMenu() {
+        System.out.println(MENU_CHAT_1_1);
+        System.out.println(MENU_CHAT_1_2);
+        System.out.println(MENU_CHAT_EXIT);
+    }
+
+    private void showSettingsChatMenu() {
+        System.out.println(MENU_CHAT_1_2_1);
+        System.out.println(MENU_CHAT_1_2_2);
+        System.out.println(MENU_CHAT_1_2_3);
+        System.out.println(MENU_CHAT_EXIT);
     }
 
     private void selectMainAction(String op) {
         Msg msgOut = null;
         switch (op) {
+            // Show all users online
             case OP_1:
                 msgOut = ClientAPI.newRequest().showAllOnline();
                 break;
+            // Start single chat (1v1)
             case OP_2:
                 msgOut = ClientAPI.newRequest().askForSingle(c.getConId(), selectSingle(), c.getNick());
                 break;
+            // Start Multiple Chat Menu
             case OP_3:
+                showChatMenu();
+                selectChatAction(sc.nextLine());
                 break;
+            // Exit program
             case OP_EXIT:
                 System.exit(0);
             default:
-                if (OP_YES.equals(op)) {
+                if (OP_ALLOW.equals(op)) {
                     msgOut = ClientAPI.newRequest().permissionRespond(true, singleId, c.getConId(), c.getNick());
-                } else if (OP_NO.equals(op)) {
+                } else if (OP_DENY.equals(op)) {
                     inSingleChat = false;
                     msgOut = ClientAPI.newRequest().permissionRespond(false, singleId, c.getConId(), c.getNick());
+                } else {
+
                 }
                 break;
         }
@@ -101,7 +143,7 @@ public class ConsoleConnection extends Thread implements ApiCodes, ConsoleAction
     }
 
     private void selectSingleAction(String op) {
-        if (op.equals(ACTION_EXIT_SINGLE)) {
+        if (op.equals(MENU_SINGLE_EXIT)) {
             exitSingle();
             inSingleChat = false;
             singleId = null;
@@ -113,6 +155,24 @@ public class ConsoleConnection extends Thread implements ApiCodes, ConsoleAction
         }
     }
 
+    private void selectChatAction(String op) {
+        switch (op) {
+            // Enter chat
+            case OP_1:
+                showInChatMenu();
+                break;
+            // Create chat
+            case OP_2:
+                showSettingsChatMenu();
+                break;
+            // Delete chat
+            case OP_EXIT:
+                break;
+            default:
+                break;
+        }
+    }
+
     private void exitSingle() {
         c.writeMessage(ClientAPI.newRequest().exitSingle(c.getConId(), singleId));
     }
@@ -120,15 +180,31 @@ public class ConsoleConnection extends Thread implements ApiCodes, ConsoleAction
     private String selectSingle() {
         int userID;
         do {
-            System.out.println(ACTION_SELECT_USER_BY_ID);
+            System.out.println(IN_SELECT_USER);
             c.writeMessage(ClientAPI.newRequest().showAllOnline());
             try {
                 userID = Integer.parseInt(sc.nextLine());
             } catch (InputMismatchException | NumberFormatException e) {
-                System.out.println(ACTION_SELECT_SINGLE);
+                System.out.println(IN_SELECT_SINGLE);
                 continue;
             }
             return String.valueOf(userID);
+        } while (true);
+    }
+
+    private String selectChat() {
+        int chatID;
+
+        do {
+            System.out.println(IN_SELECT_CHAT);
+            c.writeMessage(ClientAPI.newRequest().showAllOnline());
+            try {
+                chatID = Integer.parseInt(sc.nextLine());
+            } catch (InputMismatchException | NumberFormatException e) {
+                System.out.println(IN_SELECT_SINGLE);
+                continue;
+            }
+            return String.valueOf(chatID);
         } while (true);
     }
 
@@ -147,8 +223,8 @@ public class ConsoleConnection extends Thread implements ApiCodes, ConsoleAction
                 singleNick = reqRespond.getParameter(0);
                 singleId = reqRespond.getEmisor();
                 System.out.println("--" + singleNick + " wants to chat with you--");
-                System.out.println(MENU_ALLOW_SINGLE);
-                System.out.println(MENU_DENY_SINGLE);
+                System.out.println(MENU_SINGLE_ALLOW);
+                System.out.println(MENU_SINGLE_DENY);
                 break;
             case REQ_WAITING_FOR_PERMISSION:
                 System.out.println(reqRespond.getParameter(0) + _PENDING_TO_ACCEPT);
@@ -187,7 +263,6 @@ public class ConsoleConnection extends Thread implements ApiCodes, ConsoleAction
 
     private void handleError(Msg respondError) {
         changeConsoleColor(ConsoleColor.RED);
-        // System.out.println(respondError.toString());
         switch (respondError.getAction()) {
             case ERROR_CLIENT_NOT_FOUND:
                 System.out.println("There is no client with that ID");
